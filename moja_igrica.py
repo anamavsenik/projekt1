58e7ac95a7b8d182ebf2b3cc30326c10838f4c54
Ana Marija Kravanja <anamarija.kravanja@gmail.com>
2017-06-10 18:18:28 +0200

    sprememba

--- moja_igrica.py
+++ moja_igrica.py
@@ -68,6 +68,7 @@ def zapisi_rezultat():
     f = open('rezultati.txt', 'a')
     f.write(',' + str(stevec))
     f.close()
+    
 def preberi_najboljsi_rezultat():
     tabela = []
     with open('rezultati.txt', 'r') as f:
@@ -86,8 +87,9 @@ def skrci_levo(s):
         if i !=0:
             seznam.append(i)
     seznam = skrci_levo_brez_nicel(seznam)
-    return seznam + [0]*(len(s)-len(seznam)) 
+    return seznam + [0]*(len(s)-len(seznam))
 
+    
 def skrci_desno(s):
     return skrci_levo(s[::-1])[::-1]
 
@@ -149,65 +151,25 @@ class Matrika:
         self.osvezi()
         
     def levi_klik(self,event):
-        matrika2= skrci_matriko(self.matrika,1)
-        if matrika2 != self.matrika:
-            self.matrika=matrika2
-            self.matrika = dodaj_dve(self.matrika)
-        else:
-            stevilo_nicel=0
-            for i in range(4):
-                for j in range(4):
-                    if self.matrika[i][j] ==0:
-                        stevilo_nicel+= 1
-            if stevilo_nicel==0:
-                self.slika.create_text(100,100,text='IZGUBILI STE!',font='arial 30')
+        self.matrika = skrci_matriko(self.matrika,1)
+        self.matrika = dodaj_dve(self.matrika)
         self.osvezi()
         
     def desni_klik(self,event):
-        matrika2= skrci_matriko(self.matrika,2)
-        if matrika2 != self.matrika:
-            self.matrika=matrika2
-            self.matrika = dodaj_dve(self.matrika)
-        else:
-            stevilo_nicel=0
-            for i in range(4):
-                for j in range(4):
-                    if self.matrika[i][j] ==0:
-                        stevilo_nicel+= 1
-            if stevilo_nicel==0:
-                self.slika.create_text(100,100,text='IZGUBILI STE!',font='arial 30')
+        self.matrika = skrci_matriko(self.matrika,2)
+        self.matrika = dodaj_dve(self.matrika)
         self.osvezi()
         
     def gor_klik(self,event):
-        matrika2= skrci_matriko(self.matrika,3)
-        if matrika2 != self.matrika:
-            self.matrika=matrika2
-            self.matrika = dodaj_dve(self.matrika)
-        else:
-            stevilo_nicel=0
-            for i in range(4):
-                for j in range(4):
-                    if self.matrika[i][j] ==0:
-                        stevilo_nicel+= 1
-            if stevilo_nicel==0:
-                self.slika.create_text(100,100,text='IZGUBILI STE!',font='arial 30')
+        self.matrika = skrci_matriko(self.matrika,3)
+        self.matrika = dodaj_dve(self.matrika)
         self.osvezi()
         
+        
     def dol_klik(self,event):
-        matrika2= skrci_matriko(self.matrika,4)
-        if matrika2 != self.matrika:
-            self.matrika=matrika2
-            self.matrika = dodaj_dve(self.matrika)
-        else:
-            stevilo_nicel=0
-            for i in range(4):
-                for j in range(4):
-                    if self.matrika[i][j] ==0:
-                        stevilo_nicel+= 1
-            if stevilo_nicel==0:
-                self.slika.create_text(100,100,text='IZGUBILI STE!',font='arial 30')
+        self.matrika = skrci_matriko(self.matrika,4)
+        self.matrika = dodaj_dve(self.matrika)
         self.osvezi()
-
 
 
 okno = tk.Tk()
@@ -219,11 +181,11 @@ sestevek_tock.place(relx=.7,rely=.01)
 matrika = Matrika(okno)
 gumb_nova_igra = tk.Button(okno,text = 'Nova igra',command = matrika.nova_igra)
 gumb_nova_igra.place(relx=.02,rely=.09)
-gumb_najboljsi_rezultat = tk.Label(okno,text = 'Najboljsi rezultat : ' + preberi_najboljsi_rezultat())
-gumb_najboljsi_rezultat.place(relx=.6,rely=.09)
+gumb_najboljsi_rezultat = tk.Label(okno,text = 'Najboljsi rezultat : ' + preberi_najboljsi_rezultat(),font='arial 12')
+gumb_najboljsi_rezultat.place(relx=.55,rely=.09)
 stanje = tk.Label(okno, text ='', font='arial 18')
 stanje.place(relx=.25 ,rely=.02)
 okno.title("2048")
 
 okno.geometry('{}x{}'.format(410, 500))
-okno.mainloop()     
+okno.mainloop()
